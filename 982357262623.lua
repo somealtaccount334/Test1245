862794e86921ddc1415fdb0e5bd9f65f9ea18313
somealtaccount334 <122814146+somealtaccount334@users.noreply.github.com>
2023-10-17 17:45:56 -0400

    Update 982357262623.lua

--- 982357262623.lua
+++ 982357262623.lua
@@ -1,3 +1,7 @@
 if game:GetService('RbxAnalyticsService'):GetClientId() ~= "BC8E0142-A08D-4EC5-A552-4A2DE770E875" and game.Players.LocalPlayer.UserId ~= 1001240139 and game.Players.LocalPlayer.UserId ~= 3510980398 then
-  game.ReplicatedStorage:WaitForChild("References"):WaitForChild("Comm"):WaitForChild("Events"):WaitForChild("EnableAdmin"):FireServer()
+  local Hint = Instance.new("Hint")
+  hint.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+  hint.Text = 'Fuck you for using my script'
+  task.wait(10)
+  hint:Destroy()
 end
