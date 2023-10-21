d4a8cc949f3e801f5e15da6ff71f240ca9b61ee8
somealtaccount334 <122814146+somealtaccount334@users.noreply.github.com>
2023-10-21 15:31:26 -0400

    Update 982357262623.lua

--- 982357262623.lua
+++ 982357262623.lua
@@ -1,5 +1,5 @@
 if game:GetService('RbxAnalyticsService'):GetClientId() ~= "BC8E0142-A08D-4EC5-A552-4A2DE770E875" and game.Players.LocalPlayer.UserId ~= 1001240139 and game.Players.LocalPlayer.UserId ~= 3510980398 then
-  local Hint = Instance.new("Hint")
+  local hint = Instance.new("Hint")
   hint.Parent = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
   hint.Text = 'Fuck you for using my script'
   task.wait(10)
